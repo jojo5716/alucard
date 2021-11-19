0e484117893893b2785da76f2243042c985337c5
Jonathan Rodriguez <jrodriguez.5716@gmail.com>
2021-11-19 23:33:34 +0100

    first commit

---
+++ src/main/kotlin/com/goodcode/alucard/AlucardApplication.kt
@@ -0,0 +1,11 @@
+package com.goodcode.alucard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlucardApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlucardApplication>(*args)
+}
---
+++ src/test/kotlin/com/goodcode/alucard/AlucardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.goodcode.alucard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlucardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
