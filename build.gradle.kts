490358ba616b9cceb9d80ccf3447eeb9e5fd31f0
Jonathan Rodriguez <jrodriguez.5716@gmail.com>
2021-11-22 22:36:49 +0100

    Remove kafka configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,6 @@ group = "com.goodcode"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 val jacksonVersion = "2.9.6"
-val kafkaVersion = "2.6.0"
 
 //avro {
 //	fieldVisibility.set("PRIVATE")
@@ -33,12 +32,6 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.postgresql:postgresql")
-
-//	implementation("org.springframework.kafka:spring-kafka")
-//	implementation("org.springframework.kafka:spring-kafka-test")
-//	implementation("org.apache.kafka:kafka-clients:$kafkaVersion")
-//	implementation("org.springframework.boot:spring-boot-starter-tomcat")
-//	implementation("io.projectreactor.kafka:reactor-kafka:1.2.2.RELEASE")
 
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
